f4e6946aa5c4a5107379aedf95cdbc34c407f53e
Jonathan Voss <jvoss@onvox.net>
2011-02-01 13:26:55 -0500

    Added basic README

--- lib/log4r-xmpp/xmppoutputter.rb
+++ lib/log4r-xmpp/xmppoutputter.rb
@@ -1,3 +1,21 @@
+# Copyright 2011 Jonathan P. Voss
+#
+# This file is part of Log4r-XMPP
+#
+# Log4r-XMPP is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# Log4r-XMPP is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Log4r-XMPP. If not, see <http://www.gnu.org/licenses/>.
+#
+
 require 'log4r'
 require 'xmpp4r/client'
 
@@ -84,7 +102,7 @@ module Log4r
 
       @username = hash[:username]
       @password = hash[:password]
-      @resource = hash[:resource]
+      @resource = hash[:resource] ||= 'Log4r'
 
       @recipients = hash[:recipients]
 
